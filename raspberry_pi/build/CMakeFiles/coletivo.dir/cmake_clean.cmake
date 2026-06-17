753263f849e68aab5b9ee6805cf89b5c18f9486b
vinicius-landim <vinicius.landim@unifesp.br>
2026-06-17 13:55:22 -0300

    fix stepMotor limits

---
+++ raspberry_pi/build/CMakeFiles/coletivo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coletivo.dir/link.d"
+  "CMakeFiles/coletivo.dir/src/main.cpp.o"
+  "CMakeFiles/coletivo.dir/src/main.cpp.o.d"
+  "coletivo"
+  "coletivo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/coletivo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ raspberry_pi/build/CMakeFiles/coletivo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for coletivo.
+# This may be replaced when dependencies are built.
